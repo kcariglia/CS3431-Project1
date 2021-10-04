3d529f484486ae41ce7e4d3089d3494fb3cfdf5c
kcariglia <52259573+kcariglia@users.noreply.github.com>
2021-10-04 12:12:04 -0400

    Add files via upload

---
+++ p3/src/p3.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class p3 {
+    public static void main(String[] args) {
+
+        if (args.length <= 4) {
+            System.out.println("You need to include your UserID and Password parameters on the command line");
+            return;
+        }
+        else{
+            String username = args[3];
+            String password = args[4];
+
+            System.out.println("Include the number of the following menu item as the third parameter on the command line.\n1 – Report Participant Information\n2 – Report Pottery Information\n3 – Report Building Galleries Information\n4 – Update Member ID");
+        }
+    }
+}
