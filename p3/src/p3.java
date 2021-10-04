79843df7078c04bbd0fff1a07c8f4e1001d3e9d6
kcariglia <52259573+kcariglia@users.noreply.github.com>
2021-10-04 18:22:43 -0400

    Update p3.java

--- p3/src/p3.java
+++ p3/src/p3.java
@@ -3,16 +3,36 @@ import java.util.Scanner;
 
 public class p3 {
     public static void main(String[] args) {
-
-        if (args.length <= 4) {
+        System.out.println(args.length);
+        if (args.length < 2) {
             System.out.println("You need to include your UserID and Password parameters on the command line");
-            return;
-        }
-        else{
-            String username = args[2];
-            String password = args[3];
+            return;}
 
-            System.out.println("Include the number of the following menu item as the third parameter on the command line.\n1 – Report Participant Information\n2 – Report Pottery Information\n3 – Report Building Galleries Information\n4 – Update Member ID");
+        String username = args[0];
+        String password = args[1];
+
+        System.out.println("Include the number of the following menu item as the third parameter on the command line." +
+                    "1 – Report Participant Information" +
+                    "2 – Report Pottery Information" +
+                    "3 – Report Building Galleries Information" +
+                    "4 – Update Member ID");
+
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        int option = scanner.nextInt();
+
+        if(option == 1){
+
         }
+        if(option == 2){
+
+        }
+        if(option == 3){
+
+        }
+        if(option == 4){
+
+        }
+
     }
 }
