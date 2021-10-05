ff962b450c4769aaf87f5c5f0f23866bad6671e7
kcariglia <52259573+kcariglia@users.noreply.github.com>
2021-10-05 09:06:41 -0400

    Update p3.java

--- p3/src/p3.java
+++ p3/src/p3.java
@@ -11,6 +11,27 @@ public class p3 {
         String username = args[0];
         String password = args[1];
 
+        //System.out.println("-------Oracle JDBC COnnection Testing ---------");
+        try {
+            // Register the Oracle driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        } catch (ClassNotFoundException e){
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+        Connection connection = null;
+        try {
+            // create the connection string
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl", username, password);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
         System.out.println("Include the number of the following menu item as the third parameter on the command line." +
                     "1 – Report Participant Information" +
                     "2 – Report Pottery Information" +
@@ -21,14 +42,58 @@ public class p3 {
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         int option = scanner.nextInt();
 
-        if(option == 1){
+        DBConnect.connect(username, password);
 
+        if(option == 1){
+            //query 1 goes here
         }
         if(option == 2){
-
+            //query 2 goes here
         }
         if(option == 3){
+            System.out.println("Enter Building Name: ");
+            String building = scanner.nextLine();
 
+            try {
+                Statement st3 = connection.createStatement();
+                String query3 = "select buildingName, street, city, state, zipcode, galleryName\n" +
+                        "from Building natural join Gallery\n" +
+                        "where Building.buildingName = \n" +
+                        "group by buildingName " + building + "\n" +
+                        "order by galleryName desc;";
+                ResultSet r3 = st3.executeQuery(query3);
+
+                String street;
+                String city;
+                String state;
+                String zipcode;
+                String galleryName;
+                int numgal = 1;
+
+                while(r3.next()){
+                    street = r3.getString("street");
+                    city = r3.getString("city");
+                    state = r3.getString("state");
+                    zipcode = r3.getString("zipcode");
+                    galleryName = r3.getString("galleryName");
+
+                    System.out.println("Building Gallery Information\n" +
+                            "Building name: " + building +
+                            "Building address: " + street + " " + city + ", " + state + " " + zipcode+ "\n" +
+                            "Gallery " + Integer.toString(numgal) + ": " + galleryName);
+
+                    numgal++;
+                    r3.close();
+                    st3.close();
+                    connection.close();
+                }
+
+
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+                return;
+            }
         }
         if(option == 4){
 
@@ -36,3 +101,4 @@ public class p3 {
 
     }
 }
+
